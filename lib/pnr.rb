23781ee68ecd069fe0177d8fb0cb04b6c2dc9a9c
Felix Wetell <felix.wetell@elev.it-gymnasiet.se>
2015-05-28 16:23:02 +0200

    second part, not complete code

--- lib/pnr.rb
+++ lib/pnr.rb
@@ -8,5 +8,30 @@ def valid_pnr(pnr:)
   numbers = array.map(&:to_i)
   numbers.delete_at(6)
   p numbers
-  
+
+  if numbers.length != 10
+
+    return false
+
+  else
+    numbers.length == 10
+
+    total << numbers[-1]
+
+    while numbers.length != 0
+      p numbers
+
+      numbers[0] *= 2
+      numbers[0] -= 9
+      total << numbers[0]
+      numbers.delete_at(0)
+      total << numbers[1]
+      numbers.delete_at(1)
+
+    end
+    p total
+  end
+
 end
+
+valid_pnr(pnr: "012345-6789")
