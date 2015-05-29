587f3f4aa96d335f82ded48179005fa6f95e3731
Felix Wetell <felix.wetell@elev.it-gymnasiet.se>
2015-05-29 16:26:49 +0200

    almost working code

--- lib/pnr.rb
+++ lib/pnr.rb
@@ -1,37 +1,41 @@
 require 'pry'
 require 'pry-byebug'
 
-def valid_pnr(pnr:)
+def valid_pnr?(pnr:)
 
   total = 0
   array = pnr.split(//)
   numbers = array.map(&:to_i)
-  numbers.delete_at(6)
-  p numbers
+  if numbers.empty?
+    raise ArgumentError
+  end
 
-  if numbers.length != 10
+  if numbers.length != 11
 
     return false
 
   else
-    numbers.length == 10
 
-    total << numbers[-1]
+    numbers.delete_at(6)
+    total += numbers[-1]
+    numbers.delete_at(-1)
 
     while numbers.length != 0
-      p numbers
-
-      numbers[0] *= 2
-      numbers[0] -= 9
-      total << numbers[0]
+      if numbers[0] > 5
+        numbers[0] *= 2
+        numbers[0] -= 9
+        total += numbers[0]
+      else
+        total += numbers[1]
+      end
       numbers.delete_at(0)
-      total << numbers[1]
       numbers.delete_at(1)
 
     end
-    p total
+
+  end
+    return false
   end
 
 end
 
-valid_pnr(pnr: "012345-6789")
